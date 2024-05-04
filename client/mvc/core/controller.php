61f33ad13024ffdc62f37b0035decdf623bbece6
lehan1410 <lehan14102004@gmail.com>
2024-05-04 16:28:25 +0700

    m

--- client/mvc/core/controller.php
+++ client/mvc/core/controller.php
@@ -1,5 +1,7 @@
 <?php
 class controller{
-    
+    static function view($view, $data=[]){
+        include_once "./mvc/views/".$view.".php";
+    }
 }
 ?>
---
+++ client/mvc/models/database.php
@@ -0,0 +1,29 @@
+<?php
+
+class Database
+{
+	public $conn = NULL;
+	private $server = 'localhost';
+	private $dbName = 'Ecommerce-Website';
+	private $user = 'root';
+	private $password = '';
+        
+	public function connect()
+	{
+		$this->conn = new mysqli($this->server, $this->user, $this->password, $this->dbName);
+
+		if ($this->conn->connect_error) {
+			printf($this->conn->connect_error);
+			exit();
+		}
+		$this->conn->set_charset("utf8");
+	}
+
+        public function closeDatabase()
+	{
+		if ($this->conn) {
+			$this->conn->close();
+		}
+	}
+}
+?>
